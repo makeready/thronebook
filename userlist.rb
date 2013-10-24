c03085719d1e2d763062f8791074542025395455
makeready <shotsinthedark@gmail.com>
2013-10-24 16:12:45 -0400

    Fixed Game of Thronescore method

--- userlist.rb
+++ userlist.rb
@@ -38,7 +38,8 @@ class Userlist
 
   def game_of_thronebook
     sort_users("thronescore")
-    puts users[0].fname + " " + users[0].house
+    puts users[0].fname + " " + users[0].house + " has the most influence and is the winner of the game of thrones!"
+    gets.chomp
   end
 
   def create_user(*args)
